93d6d1273c3decc96df81d9ff9938215628536e3
Naol Kuma <naolkuma02@gmail.com>
2023-01-24 19:27:30 +0300

    order ui completing

--- src/java/DbConnection.java
+++ src/java/DbConnection.java
@@ -3,15 +3,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author Naol
- */
 public class DbConnection {
         static final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver";
     static final String DB_URL = "jdbc:oracle:thin:@localhost:1521:xe";
